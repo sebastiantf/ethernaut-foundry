ba4c4fcde204aefb5053f39a4754a066201bf40a
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-02-14 19:06:55 +0530

    feat: ✨ add Level Hack & Submit for `Instance`

--- test/Instance.t.sol
+++ test/Instance.t.sol
@@ -10,6 +10,7 @@ import "../src/levels/Instance.sol";
 contract InstanceTest is Test {
     Ethernaut ethernaut;
     Statistics statistics;
+    address eoa = address(0x1337);
 
     function setUp() public {
         ethernaut = new Ethernaut();
@@ -29,6 +30,9 @@ contract InstanceTest is Test {
         InstanceFactory instanceFactory = new InstanceFactory();
         // Register level on Ethernaut
         ethernaut.registerLevel(instanceFactory);
+
+        // Set caller to custom address
+        vm.startPrank(eoa);
 
         // Start recording logs to capture new level instance address
         vm.recordLogs();
@@ -50,5 +54,39 @@ contract InstanceTest is Test {
 
         // Instantiate level instance
         Instance instance = Instance(instanceAddress);
+
+        /* Level Hack */
+        string memory password = instance.password();
+        emit log_named_string("password", password);
+
+        instance.authenticate(password);
+
+        /* Level Submit */
+        // Start recording logs to capture level completed log
+        vm.recordLogs();
+        ethernaut.submitLevelInstance(payable(instanceAddress));
+
+        // Parse emitted logs
+        Vm.Log[] memory submitLogsEntries = vm.getRecordedLogs();
+        assertEq(
+            submitLogsEntries[1].topics[0],
+            keccak256("LevelCompletedLog(address,address,address)")
+            // event LevelCompletedLog(address indexed player, address indexed instance, address indexed level);
+        );
+        // Cast bytes32 log arg into address
+        address playerAddress_ = address(
+            uint160(uint256(submitLogsEntries[1].topics[1]))
+        );
+        emit log_named_address("playerAddress_", playerAddress_);
+
+        address instanceAddress_ = address(
+            uint160(uint256(submitLogsEntries[1].topics[2]))
+        );
+        emit log_named_address("instanceAddress_", instanceAddress_);
+
+        assertEq(playerAddress_, eoa);
+        assertEq(instanceAddress_, instanceAddress);
+
+        vm.stopPrank();
     }
 }

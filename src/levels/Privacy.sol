829fd39566a7105d22c661d7b2e415f637aa6860
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-02-15 18:58:34 +0530

    feat: ✨ add slot comments for `Privacy`

--- src/levels/Privacy.sol
+++ src/levels/Privacy.sol
@@ -3,11 +3,19 @@ pragma solidity ^0.8.0;
 
 contract Privacy {
 
+  // slot 0
   bool public locked = true;
+  // slot 1
   uint256 public ID = block.timestamp;
+  // slot 2 - 1 byte
   uint8 private flattening = 10;
+  // slot 2 - 1 byte
   uint8 private denomination = 255;
+  // slot 2 - 2 bytes
   uint16 private awkwardness = uint16(block.timestamp);
+  // slot 3 - [0]
+  // slot 4 - [1]
+  // slot 5 - [2]
   bytes32[3] private data;
 
   constructor(bytes32[3] memory _data) {

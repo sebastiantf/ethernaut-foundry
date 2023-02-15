c8123ae6a367f1acc6f28e096e94dde363da3a40
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-02-15 18:59:18 +0530

    feat: 🧪 add hack setup for `Privacy`

---
+++ test/Privacy.t.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+import "../src/Ethernaut.sol";
+import "../src/metrics/Statistics.sol";
+import "../src/levels/PrivacyFactory.sol";
+import "../src/levels/Privacy.sol";
+
+contract PrivacyTest is Test {
+    using stdStorage for StdStorage;
+
+    Ethernaut ethernaut;
+    Statistics statistics;
+    address eoa = address(0x1337);
+
+    function setUp() public {
+        ethernaut = new Ethernaut();
+
+        // Statistics is actually deployed as upgradeable proxy
+        // Deploying and initializing it normally for simplicity
+        statistics = new Statistics();
+        statistics.initialize(address(ethernaut));
+
+        // Set Statistics contract on Ethernaut
+        ethernaut.setStatistics(address(statistics));
+    }
+
+    function testPrivacyHack() public {
+        /* Level Setup */
+        // Deploy level factory: PrivacyFactory
+        PrivacyFactory privacyFactory = new PrivacyFactory();
+        // Register level on Ethernaut
+        ethernaut.registerLevel(privacyFactory);
+
+        // Set caller to custom address
+        vm.startPrank(eoa);
+
+        // Start recording logs to capture new level instance address
+        vm.recordLogs();
+        // Create new level instance via Ethernaut
+        ethernaut.createLevelInstance(privacyFactory);
+
+        // Parse emitted logs
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        assertEq(
+            entries[0].topics[0],
+            keccak256("LevelInstanceCreatedLog(address,address,address)")
+            // event LevelInstanceCreatedLog(address indexed player, address indexed instance, address indexed level);
+        );
+        Vm.Log memory levelPrivacyCreatedLog = entries[0];
+
+        // Cast bytes32 log arg into address
+        address instanceAddress = address(
+            uint160(uint256(levelPrivacyCreatedLog.topics[2]))
+        );
+        emit log_named_address("instanceAddress", instanceAddress);
+
+        // Instantiate level instance
+        Privacy instance = Privacy(payable(instanceAddress));
+
+        /* Level Hack */
+
+        /* Level Submit */
+        // Start recording logs to capture level completed log
+        vm.recordLogs();
+        ethernaut.submitLevelInstance(payable(instanceAddress));
+
+        // Parse emitted logs
+        Vm.Log[] memory submitLogsEntries = vm.getRecordedLogs();
+        assertEq(
+            submitLogsEntries[1].topics[0],
+            keccak256("LevelCompletedLog(address,address,address)")
+            // event LevelCompletedLog(address indexed player, address indexed instance, address indexed level);
+        );
+        Vm.Log memory levelCompletedLog = submitLogsEntries[1];
+
+        // Cast bytes32 log arg into address
+        address playerAddress_ = address(
+            uint160(uint256(levelCompletedLog.topics[1]))
+        );
+        emit log_named_address("playerAddress_", playerAddress_);
+
+        address instanceAddress_ = address(
+            uint160(uint256(levelCompletedLog.topics[2]))
+        );
+        emit log_named_address("instanceAddress_", instanceAddress_);
+
+        assertEq(playerAddress_, eoa);
+        assertEq(instanceAddress_, instanceAddress);
+
+        vm.stopPrank();
+    }
+}

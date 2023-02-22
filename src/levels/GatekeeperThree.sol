aeb0ecfdc96e45182e585cccae62d3c4bae5d1b5
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-02-22 12:26:05 +0530

    feat: ✨ add Level Setup for `GatekeeperThree`

---
+++ src/levels/GatekeeperThree.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleTrick {
+  GatekeeperThree public target;
+  address public trick;
+  uint private password = block.timestamp;
+
+  constructor (address payable _target) {
+    target = GatekeeperThree(_target);
+  }
+    
+  function checkPassword(uint _password) public returns (bool) {
+    if (_password == password) { // password would be block.timestamp of contract deployment. or read from storage
+      return true;
+    }
+    password = block.timestamp;
+    return false;
+  }
+    
+  function trickInit() public {
+    trick = address(this);
+  }
+    
+  function trickyTrick() public {
+    if (address(this) == msg.sender && address(this) != trick) {
+      target.getAllowance(password);
+    }
+  }
+}
+
+contract GatekeeperThree {
+  address public owner;
+  address public entrant;
+  bool public allow_enterance = false;
+  SimpleTrick public trick;
+
+  function construct0r() public {
+      owner = msg.sender;
+  }
+
+  modifier gateOne() {
+    require(msg.sender == owner); // means caller contract should be owner. can be achieved by calling construct0r() 
+    require(tx.origin != owner); // means caller should be contract not eoa
+    _;
+  }
+
+  modifier gateTwo() {
+    require(allow_enterance == true); // need to set enterance via trick.checkPassword(_password)
+    _;
+  }
+
+  modifier gateThree() {
+    if (address(this).balance > 0.001 ether && payable(owner).send(0.001 ether) == false) {
+      _;
+    }
+  }
+
+  function getAllowance(uint _password) public {
+    if (trick.checkPassword(_password)) { // password would be contract deployment block.timestamp of SimpleTrick. Hence need to call createTrick()
+        allow_enterance = true;
+    }
+  }
+
+  function createTrick() public {
+    trick = new SimpleTrick(payable(address(this)));
+    trick.trickInit();
+  }
+
+  function enter() public gateOne gateTwo gateThree returns (bool entered) {
+    entrant = tx.origin;
+    return true;
+  }
+
+  receive () external payable {}
+}
